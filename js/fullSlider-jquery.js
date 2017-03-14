dbd8305ee38b5735b9bc8a360491a2fa25189459
bbykwiatu <bkwiatkowski@op.pl>
2017-03-14 15:44:46 +0100

    add options

--- js/fullSlider-jquery.js
+++ js/fullSlider-jquery.js
@@ -2,11 +2,11 @@
 
 (function($) {
 
-	$.fn.fullSlider = function() {
+	$.fn.fullSlider = function(params) {
 		
 		if (this.length > 1) {
 			this.each(function() {
-				$(this).fullSlider();
+				$(this).fullSlider(params);
 			});
 			return this;
 		}
@@ -14,7 +14,13 @@
 		var slider = $(this),
 			items,
 			inner,
-			scrollWidth;
+			scrollWidth,
+			timer,
+			options = $.extend({
+				speed: .5,
+				after: function() {},
+				before: function() {}
+			}, params);
 		
 		var init = function() {
 			
@@ -39,10 +45,24 @@
 				return;
 			}
 			
+			setStyle();
 			setOffset();
 		};
 		
+		
 		init();
+		
+		
+		function setStyle() {
+			slider.find('.scroll-bar').css({
+				transition: 'transform ' + options.speed + 's'
+			});
+			
+			slider.find('.item').css({
+				transition: 'all ' + options.speed + 's cubic-bezier(.68,-0.55,.27,1.55)'
+			});
+		}
+		
 		
 		function scroll(index) {
 			slider.find('.scroll-bar').css({
@@ -80,21 +100,48 @@
 				itemIndex = items.length-1;
 			}
 			
+			if(typeof options.before === 'function') {
+				options.before(item);
+			}
+			
 			items
 				.removeClass('active')
 				.parent()
 				.find('.item')
 				.eq(itemIndex)
 				.addClass('active');
-				
+			
 			scroll(itemIndex);
+			
+			if(typeof options.after === 'function') {
+				
+				clearTimeout(timer);
+				
+				timer = setTimeout(function() {
+					options.after(items.parent().find('.item').eq(itemIndex));
+				}, options.speed * 1000);
+			}
 		});
 		
 		items.click(function() {
 			var that = $(this);
+			
+			if(typeof options.before === 'function') {
+				options.before(items.parent().find('.active'));
+			}
+			
 			items.removeClass('active');
 			that.addClass('active');
 			scroll(that.index());
+			
+			if(typeof options.after === 'function') {
+				
+				clearTimeout(timer);
+				
+				timer = setTimeout(function() {
+					options.after(that);
+				}, options.speed * 1000);
+			}
 		});
 		
 		$(window).resize(function(e) {
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 (function() {
-	$('.full-slider').fullSlider();
+	$('.full-slider').fullSlider({
+		speed: 1,
+		before: function(item) {
+			console.log('before');
+		},
+		after: function(item) {
+			console.log('after');
+		}
+	});
 })();
